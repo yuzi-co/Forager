157352045ad677edb1a0dadbd74395d1142ab318
yuzi-co <vadim@yuzi.co>
2019-01-14 19:26:22 +0200

    update to default autostart files

--- AutoExchange.bat
+++ AutoExchange.bat
@@ -3,7 +3,7 @@
 cd /d %~dp0
 
 set Mode=Automatic
-set Pools=MiningPoolHub,NiceHash,Zpool,BlockMasters
+set Pools=NiceHash,Zpool,BlockMasters,NLPool
 
 powershell -version 5.0 -noexit -executionpolicy bypass -command "& .\Core.ps1 -MiningMode %Mode% -PoolsName %Pools%"
 ::pwsh -noexit -executionpolicy bypass -command "& .\Core.ps1 -MiningMode %Mode% -PoolsName %Pools%"
