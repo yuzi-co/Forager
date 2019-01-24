1ad2fa08e492cc52d6d67dcdf825b22a3770af6b
yuzi-co <vadim@yuzi.co>
2019-01-24 21:20:25 +0200

    disable cryptopia and update wtm info for manual mining

--- Miner.ps1
+++ Miner.ps1
@@ -139,18 +139,25 @@ if ($MiningMode -ne "FARM MONITORING") {
                 $CMCResponse = try { Invoke-RestMethod -Uri "https://api.coinmarketcap.com/v1/ticker/?limit=0" -UseBasicParsing -TimeoutSec 10 } catch { $null; Write-Host "Not responding" }
                 'Calling Bittrex API' | Write-Host
                 $BTXResponse = try { Invoke-RestMethod -Uri "https://bittrex.com/api/v1.1/public/getmarketsummaries" -UseBasicParsing -TimeoutSec 10 | Select-Object -ExpandProperty 'result' } catch { $null; Write-Host "Not responding" }
-                'Calling Cryptopia API' | Write-Host
-                $CRYResponse = try { Invoke-RestMethod -Uri "https://www.cryptopia.co.nz/api/GetMarkets/BTC" -UseBasicParsing -TimeoutSec 10 | Select-Object -ExpandProperty 'data' } catch { $null; Write-Host "Not responding" }
+                # 'Calling Cryptopia API' | Write-Host
+                # $CRYResponse = try { Invoke-RestMethod -Uri "https://www.cryptopia.co.nz/api/GetMarkets/BTC" -UseBasicParsing -TimeoutSec 10 | Select-Object -ExpandProperty 'data' } catch { $null; Write-Host "Not responding" }
 
                 #Add main page coins
                 $WtmUrl = 'https://whattomine.com/coins.json?' +
+                'bcd=true&factor[bcd_hr]=10&factor[bcd_p]=0&' + #BCD
                 'bk14=true&factor[bk14_hr]=10&factor[bk14_p]=0&' + #Decred
                 'cn=true&factor[cn_hr]=10&factor[cn_p]=0&' + #CryptoNight
                 'cn7=true&factor[cn7_hr]=10&factor[cn7_p]=0&' + #CryptoNightV7
+                'cn8=true&factor[cn8_hr]=10&factor[cn8_p]=0&' + #CryptoNightV8
+                'cnf=true&factor[cnf_hr]=10&factor[cnf_p]=0&' + #CryptoNightFast
                 'cnh=true&factor[cnh_hr]=10&factor[cnh_p]=0&' + #CryptoNightHeavy
+                'cnhn=true&factor[cnhn_hr]=10&factor[cnhn_p]=0&' + #CryptoNightHaven
+                'cns=true&factor[cns_hr]=10&factor[cns_p]=0&' + #CryptoNightSaber
                 'eq=true&factor[eq_hr]=10&factor[eq_p]=0&' + #Equihash
+                'eqa=true&factor[eqa_hr]=10&factor[eqa_p]=0&' + #AION (Equihash210)
                 'eth=true&factor[eth_hr]=10&factor[eth_p]=0&' + #Ethash
                 'grof=true&factor[gro_hr]=10&factor[gro_p]=0&' + #Groestl
+                'hx=true&factor[hx_hr]=10&factor[hx_p]=0&' + #Hex
                 'l2z=true&factor[l2z_hr]=10&factor[l2z_p]=0&' + #Lyra2z
                 'lbry=true&factor[lbry_hr]=10&factor[lbry_p]=0&' + #Lbry
                 'lre=true&factor[lrev2_hr]=10&factor[lrev2_p]=0&' + #Lyra2v2
@@ -159,10 +166,14 @@ if ($MiningMode -ne "FARM MONITORING") {
                 'pas=true&factor[pas_hr]=10&factor[pas_p]=0&' + #Pascal
                 'phi=true&factor[phi_hr]=10&factor[phi_p]=0&' + #PHI
                 'phi2=true&factor[phi2_hr]=10&factor[phi2_p]=0&' + #PHI2
+                'ppw=true&factor[ppw_hr]=10&factor[ppw_p]=0&' #ProgPOW
                 'skh=true&factor[skh_hr]=10&factor[skh_p]=0&' + #Skunk
+                'tt10=true&factor[tt10_hr]=10&factor[tt10_p]=0&' #TimeTravel10
                 'x11gf=true&factor[x11g_hr]=10&factor[x11g_p]=0&' + #X11gost
                 'x16r=true&factor[x16r_hr]=10&factor[x16r_p]=0&' + #X16r
-                'xn=true&factor[xn_hr]=10&factor[xn_p]=0' #Xevan
+                'x22i=true&factor[x22i_hr]=10&factor[x22i_p]=0&' + #X22i
+                'xn=true&factor[xn_hr]=10&factor[xn_p]=0&' #Xevan
+                'zh=true&factor[zh_hr]=10&factor[zh_p]=0' #ZHash (Equihash144)
 
                 'Calling WhatToMine API' | Write-Host
                 $WTMResponse = try { Invoke-RestMethod -Uri $WtmUrl -UseBasicParsing -TimeoutSec 10 | Select-Object -ExpandProperty coins } catch { $null; Write-Host "Not responding" }
@@ -178,19 +189,19 @@ if ($MiningMode -ne "FARM MONITORING") {
 
                     $PriceCMC = [decimal]($CMCResponse | Where-Object Symbol -eq $Coin.Symbol | ForEach-Object {if ($(Get-CoinUnifiedName $_.Id) -eq $Coin.Info) {$_.price_btc} })
                     $PriceBTX = [decimal]($BTXResponse | Where-Object MarketName -eq ('BTC-' + $Coin.Symbol) | Select-Object -ExpandProperty Last)
-                    $PriceCRY = [decimal]($CRYResponse | Where-Object Label -eq ($Coin.Symbol + '/BTC') | Select-Object -ExpandProperty LastPrice)
+                    # $PriceCRY = [decimal]($CRYResponse | Where-Object Label -eq ($Coin.Symbol + '/BTC') | Select-Object -ExpandProperty LastPrice)
 
                     if ($PriceCMC -gt 0) {
                         $Coin.BTCPrice = $PriceCMC
                     } elseif ($PriceBTX -gt 0) {
                         $Coin.BTCPrice = $PriceBTX
-                    } elseif ($PriceCRY -gt 0) {
-                        $Coin.BTCPrice = $PriceCRY
+                        # } elseif ($PriceCRY -gt 0) {
+                        #     $Coin.BTCPrice = $PriceCRY
                     }
 
                     Remove-Variable PriceCMC
                     Remove-Variable PriceBTX
-                    Remove-Variable PriceCRY
+                    # Remove-Variable PriceCRY
 
                     #Data from WTM
                     if ($WTMResponse -ne $null) {
@@ -198,26 +209,47 @@ if ($MiningMode -ne "FARM MONITORING") {
                         if ($WtmCoin -ne $null) {
 
                             $WTMFactor = switch ($Coin.Algorithm) {
-                                "Bitcore" { 1000000 }
-                                "Blake2s" { 1000000 }
-                                "CnLiteV7" { 1 }
-                                "CnV7" { 1 }
+                                "Allium" { 1e6 }
+                                "BCD" { 1e6 }
+                                "Bitcore" { 1e6 }
+                                "Blake2s" { 1e6 }
+                                "CnFast" { 1 }
+                                "CnHaven" { 1 }
                                 "CnHeavy" { 1 }
-                                "Equihash" { 1 }
-                                "Ethash" { 1000000 }
-                                "Keccak" { 1000000 }
-                                "KeccakC" { 1000000 }
-                                "Lyra2v2" {1000}
-                                "Lyra2z" { 1000000 }
-                                "NeoScrypt" { 1000 }
-                                "Phi" { 1000000 }
-                                "Phi2" { 1000000 }
-                                "Skunk" { 1000000 }
-                                "X16r" { 1000000 }
-                                "X16s" { 1000000 }
-                                "X17" { 1000 }
+                                "CnLiteV7" { 1 }
+                                "CnSaber" { 1 }
+                                "CnV7" { 1 }
+                                "CnV8" { 1 }
+                                "Energi" { 1e6 }
+                                "Equihash144" { 1 }
+                                "Equihash192" { 1 }
+                                "Equihash210" { 1 }
+                                "Equihash96" { 1e3 }
+                                "Ethash" { 1e6 }
+                                "Hex" { 1e6 }
+                                "Keccak" { 1e6 }
+                                "KeccakC" { 1e6 }
+                                "LBK3" { 1e3 }
+                                "Lyra2v2" {1e3}
+                                "Lyra2v3" {1e3}
+                                "Lyra2z" { 1e6 }
+                                "M7M" { 1e3 }
+                                "MTP" { 1e3 }
+                                "NeoScrypt" { 1e3 }
+                                "Phi" { 1e6 }
+                                "Phi2" { 1e6 }
+                                "ProgPOW" { 1e6 }
+                                "RandomHash" { 1e3 }
+                                "Skunk" { 1e6 }
+                                "SonoA" { 1e3 }
+                                "Tensority" { 1 }
+                                "Ubqhash" { 1e6 }
+                                "X16r" { 1e6 }
+                                "X16rt" { 1e6 }
+                                "X16s" { 1e6 }
+                                "X17" { 1e3 }
+                                "X22i" { 1e6 }
                                 "Yescrypt" { 1 }
-                                "Zero" { 1 }
                                 default { $null }
                             }
 
