bdaabc0b231abe37dc959b644efdefa53edeaec3
yuzi-co <vadim@yuzi.co>
2019-03-10 11:32:53 +0200

    update mypools

--- Pools/MyPools.ps1
+++ Pools/MyPools.ps1
@@ -26,8 +26,9 @@ if ($Querymode -eq "Info") {
 if ($Querymode -eq "Core") {
     $Pools = @(
         [PSCustomObject]@{ Coin = "Dallar"     ; Symbol = "DAL"  ; Algo = "Throestl"   ; Server = "pool.dallar.org"              ; Port = 3032 ; Fee = 0.01 ; User = $Wallets.DAL }
-        [PSCustomObject]@{ Coin = "Pascalcoin" ; Symbol = "PASC" ; Algo = "RandomHash" ; Server = "mine.pool.pascalpool.org"     ; Port = 3333 ; Fee = 0.01 ; User = $Wallets.PASC + "`$(if ((`$Wallets.PASC -split '\.')[1] -eq `$null) {'.0'}).#WorkerName#/#Email#" }
+        [PSCustomObject]@{ Coin = "Pascalcoin" ; Symbol = "PASC" ; Algo = "RandomHash" ; Server = "mine.pool.pascalpool.org"     ; Port = 3333 ; Fee = 0.005 ; User = $Wallets.PASC + "`$(if ((`$Wallets.PASC -split '\.')[1] -eq `$null) {'.0'}).#WorkerName#/#Email#" }
         [PSCustomObject]@{ Coin = "Grin"       ; Symbol = "GRIN" ; Algo = "Cuckaroo29" ; Server = "eu-west-stratum.grinmint.com" ; Port = 3416 ; Fee = 0.01 ; User = '#Email#/#WorkerName#' }
+        [PSCustomObject]@{ Coin = "MicroBitcoin" ; Symbol = "MBC" ; Algo = "Rainforest" ; Server = "mbc-eu.skypool.co"          ; Port = 8001 ; Fee = 0.01 ; User = $Wallets.MBC }
     )
 
     $Result = $Pools | ForEach-Object {
