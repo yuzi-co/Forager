6930d368edfd0505f1182cddb268c592098b9cb7
yuzi-co <vadim@yuzi.co>
2019-03-15 16:41:55 +0200

    update mypools

--- Pools/MyPools.ps1
+++ Pools/MyPools.ps1
@@ -25,11 +25,12 @@ if ($Querymode -eq "Info") {
 
 if ($Querymode -eq "Core") {
     $Pools = @(
-        [PSCustomObject]@{ Coin = "Dallar"     ; Symbol = "DAL"  ; Algo = "Throestl"   ; Server = "pool.dallar.org"              ; Port = 3032 ; Fee = 0.01 ; User = $Wallets.DAL }
-        [PSCustomObject]@{ Coin = "Pascalcoin" ; Symbol = "PASC" ; Algo = "RandomHash" ; Server = "mine.pool.pascalpool.org"     ; Port = 3333 ; Fee = 0.005 ; User = $Wallets.PASC + "$(if (($Wallets.PASC -split '\.')[1] -eq $null) {'.0'}).#WorkerName#/#Email#" }
-        [PSCustomObject]@{ Coin = "Grin"       ; Symbol = "GRIN" ; Algo = "Cuckaroo29" ; Server = "eu-west-stratum.grinmint.com" ; Port = 3416 ; Fee = 0.01 ; User = '#Email#/#WorkerName#' }
-        [PSCustomObject]@{ Coin = "MicroBitcoin" ; Symbol = "MBC" ; Algo = "Rainforest" ; Server = "mbc-eu.skypool.co"          ; Port = 8001 ; Fee = 0.01 ; User = $Wallets.MBC }
-        [PSCustomObject]@{ Coin = "Verium" ; Symbol = "VRM" ; Algo = "Verium" ; Server = "eu.vrm.mining-pool.ovh"          ; Port = 3032 ; Fee = 0.009 ; User = "#UserName.#WorkerName#" }
+        [PSCustomObject]@{ Coin = "Dallar"       ; Symbol = "DAL"  ; Algo = "Throestl"   ; Server = "pool.dallar.org"              ; Port = 3032 ; Fee = 0.01  ; User = $Wallets.DAL }
+        [PSCustomObject]@{ Coin = "Pascalcoin"   ; Symbol = "PASC" ; Algo = "RandomHash" ; Server = "mine.pool.pascalpool.org"     ; Port = 3333 ; Fee = 0.005 ; User = $Wallets.PASC + "$(if (($Wallets.PASC -split '\.')[1] -eq $null) {'.0'}).#WorkerName#/#Email#" }
+        [PSCustomObject]@{ Coin = "Grin"         ; Symbol = "GRIN" ; Algo = "Cuckaroo29" ; Server = "eu-west-stratum.grinmint.com" ; Port = 3416 ; Fee = 0.01  ; User = '#Email#/#WorkerName#' }
+        [PSCustomObject]@{ Coin = "MicroBitcoin" ; Symbol = "MBC"  ; Algo = "Rainforest" ; Server = "mbc-eu.skypool.co"            ; Port = 8001 ; Fee = 0.01  ; User = $Wallets.MBC }
+        [PSCustomObject]@{ Coin = "Verium"       ; Symbol = "VRM"  ; Algo = "Verium"     ; Server = "eu.vrm.mining-pool.ovh"       ; Port = 3032 ; Fee = 0.009 ; User = "#UserName.#WorkerName#" }
+        [PSCustomObject]@{ Coin = "Verus"        ; Symbol = "VRSC" ; Algo = "Verushash"  ; Server = "eu.luckpool.net"              ; Port = 3956 ; Fee = 0.009 ; User = $Wallets.VRSC + ".#WorkerName#" }
     )
 
     $Result = $Pools | ForEach-Object {
