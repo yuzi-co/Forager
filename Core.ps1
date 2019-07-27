f36a9666688ff37c72db2b3dfc7658068a219074
yuzi-co <vadim@yuzi.co>
2019-07-27 18:42:01 +0300

    v19.07

--- Core.ps1
+++ Core.ps1
@@ -53,7 +53,7 @@ $ErrorActionPreference = "Continue"
 
 $Global:Release = @{
     Application = "Forager"
-    Version     = "19.06"
+    Version     = "19.07"
 }
 Log "$($Release.Application) v$($Release.Version)"
 
