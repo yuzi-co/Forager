82876bc36381124922f89cffdbde5b8e9118194d
yuzi-co <vadim@yuzi.co>
2020-02-01 13:43:47 +0200

    MiniZ 1.5t

--- MinersDisabled/N-MiniZ-1.5t.ps1
+++ MinersDisabled/N-MiniZ-1.5t.ps1
@@ -18,38 +18,42 @@ $M = @{
     SSL        = $true
     Fee        = 0.02
     Algorithms = [PSCustomObject]@{
+        Beam        = @{
+            Params = "--par=Beam"
+            Mem    = 3
+        }
         Equihash96  = @{
             Params = "--par=96,5 --pers auto"
             Mem    = 1.75
         }
         Equihash125 = @{
             Params = "--par=125,4"
+            Mem    = 2
         }
         Equihash144 = @{
             Params = "--par=144,5 --pers auto"
-            Mem    = 1.75
+            Mem    = 2
         }
         Equihash150 = @{
             Params = "--par=150,5"
-            Mem    = 2.9
+            Mem    = 3
         }
         Equihash192 = @{
             Params = "--par=192,7 --pers auto"
             Mem    = 2.75
         }
-        # Equihash210 = @{
-        #     Params = "--par=210,9"
-        # }
+        Equihash210 = @{
+            Params = "--par=210,9 --pers auto"
+            Mem    = 2
+        }
     }
 }
 
 if ($SystemInfo.CudaVersion -ge [version]"10.0") {
-    $M.Uri = "https://github.com/yuzi-co/miners/releases/download/2019.09/miniZ_v1.5q5_cuda10_win-x64.zip"
-    $M.SHA256 = "3799110F216D77FEADE441C1FB87CA408E37D0152B6B0D97EDC1C388DB238F69"
+    $M.Uri = "https://github.com/yuzi-co/miners/releases/download/2020.01/miniZ_v1.5t_cuda10_win-x64.zip"
     $M.CUDA = 10
 } elseif ($SystemInfo.CudaVersion -ge [version]"8.0") {
-    $M.Uri = "https://github.com/yuzi-co/miners/releases/download/2019.09/miniZ_v1.5q5_win-x64.zip"
-    $M.SHA256 = "3ED99F829C5DF49CFBB59D359C754DC8A896A7A388A81E3F691F291DBC27FC5C"
+    $M.Uri = "https://github.com/yuzi-co/miners/releases/download/2020.01/miniZ_v1.5t_cuda8_win-x64.zip"
     $M.CUDA = 8.0
 } else {
     return
--- MinersLinux/N-MiniZ-1.5t.ps1
+++ MinersLinux/N-MiniZ-1.5t.ps1
@@ -19,38 +19,42 @@ $M = @{
     SSL        = $true
     Fee        = 0.02
     Algorithms = [PSCustomObject]@{
+        Beam        = @{
+            Params = "--par=Beam"
+            Mem    = 3
+        }
         Equihash96  = @{
             Params = "--par=96,5 --pers auto"
             Mem    = 1.75
         }
         Equihash125 = @{
             Params = "--par=125,4"
+            Mem    = 2
         }
         Equihash144 = @{
             Params = "--par=144,5 --pers auto"
-            Mem    = 1.75
+            Mem    = 2
         }
         Equihash150 = @{
             Params = "--par=150,5"
-            Mem    = 2.9
+            Mem    = 3
         }
         Equihash192 = @{
             Params = "--par=192,7 --pers auto"
             Mem    = 2.75
         }
-        # Equihash210 = @{
-        #     Params = "--par=210,9"
-        # }
+        Equihash210 = @{
+            Params = "--par=210,9 --pers auto"
+            Mem    = 2
+        }
     }
 }
 
 if ($SystemInfo.CudaVersion -ge [version]"10.0") {
-    $M.Uri = "https://github.com/yuzi-co/miners-linux/releases/download/2019.09/miniZ_v1.5q5_cuda10_linux-x64.tar.gz"
-    $M.SHA256 = "E25AB2398285924DBCE749CB89E5D0A69102DA7B2C6ADFF226CFBE593A7C6961"
+    $M.Uri = "https://github.com/yuzi-co/miners-linux/releases/download/2020.01/miniZ_v1.5t_cuda10_linux-x64.tar.gz"
     $M.CUDA = 10
 } elseif ($SystemInfo.CudaVersion -ge [version]"8.0") {
-    $M.Uri = "https://github.com/yuzi-co/miners-linux/releases/download/2019.09/miniZ_v1.5q5_linux-x64.tar.gz"
-    $M.SHA256 = "5B77EA1ACE4C36ACDCDA810769A659A781696EEDC78E7B9F39FB2BB88DA8B2FE"
+    $M.Uri = "https://github.com/yuzi-co/miners-linux/releases/download/2020.01/miniZ_v1.5t_cuda8_linux-x64.tar.gz"
     $M.CUDA = 8.0
 } else {
     return
