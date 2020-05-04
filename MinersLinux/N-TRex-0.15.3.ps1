310fbf2c423239f84b190a0699c065e6f4e144a8
yuzi-co <vadim@yuzi.co>
2020-05-04 19:31:47 +0300

    TRex 0.15.3

---
+++ MinersLinux/N-TRex-0.15.3.ps1
@@ -0,0 +1,79 @@
+
+$M = @{
+    Path       = "t-rex"
+    Type       = "NVIDIA"
+    Arguments  = @(
+        "-o #Protocol#://#Server#:#Port#"
+        "-u #Login#"
+        "-p #Password#"
+        "-d #Devices#"
+        "-R 3"
+        "-r 10"
+        "--no-watchdog"
+        "--no-nvml"
+        "-b 127.0.0.1:#APIPort#"
+        "#AlgorithmParameters#"
+        "#CustomParameters#"
+    )
+    API        = "Ccminer"
+    Fee        = 0.01
+    Algorithms = [PSCustomObject]@{
+        Astralhash  = "-a astralhash"
+        Balloon     = "-a balloon"
+        BCD         = "-a bcd"
+        Bitcore     = "-a bitcore"
+        C11         = "-a c11"
+        Dedal       = "-a dedal"
+        # Hmq1725   = "-a hmq1725"
+        Honeycomb   = "-a honeycomb"
+        HSR         = "-a hsr"
+        Jeonghash   = "-a jeonghash"
+        KawPow      = "-a kawpow"
+        MTP         = @{
+            Params = "-a mtp"
+            Mem    = 4.5
+        }
+        MTPtcr      = @{
+            Params = "-a mtp-tcr"
+            Mem    = 4.5
+        }
+        Padihash    = "-a padihash"
+        Pawelhash   = "-a pawelhash"
+        Polytimos   = "-a polytimos"
+        ProgPOWSero = "-a progpow --coin sero"
+        Renesis     = "-a renesis"
+        SHA256t     = "-a sha256t"
+        SHA256q     = "-a sha256q"
+        # Skunk     = "-a skunk"
+        SonoA       = "-a sonoa"
+        Tensority   = @{
+            Params = "-a tensority"
+            Fee    = 0.03
+        }
+        Timetravel  = "-a timetravel"
+        Tribus      = "-a tribus"
+        X16r        = "-a x16r"
+        X16rt       = "-a x16rt"
+        X16rv2      = "-a x16rv2"
+        X16s        = "-a x16s"
+        X17         = "-a x17"
+        X21s        = "-a x21s"
+        X22i        = "-a x22i"
+        x25x        = "-a x25x"
+    }
+}
+
+if ($SystemInfo.CudaVersion -ge [version]"10.0") {
+    $M.Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.3/t-rex-0.15.3-linux-cuda10.0.tar.gz"
+    $M.CUDA = 10
+} elseif ($SystemInfo.CudaVersion -ge [version]"9.2") {
+    $M.Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.3/t-rex-0.15.3-linux-cuda9.2.tar.gz"
+    $M.CUDA = 9.2
+} elseif ($SystemInfo.CudaVersion -ge [version]"9.1") {
+    $M.Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.3/t-rex-0.15.3-linux-cuda9.1.tar.gz"
+    $M.CUDA = 9.1
+} else {
+    return
+}
+
+return [PSCustomObject]$M
